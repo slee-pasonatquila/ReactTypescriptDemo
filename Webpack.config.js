50d2aab9027ca3cdba48e1faf19705040a006cc6
slee_pasonatquila <slee@pasonatquila.com>
2016-03-14 10:52:28 +0900

    restart with angular2

--- Webpack.config.js
+++ Webpack.config.js
@@ -1,51 +1,112 @@
 var webpack = require('webpack');
+var helpers = require('./helpers');
+
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
+
+var ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+var HMR = helpers.hasProcessFlag('hot');
+
+var metadata = {
+  title: 'Angular2 Webpack Starter by @gdi2990 from @AngularClass',
+  baseUrl: '/',
+  host: 'localhost',
+  port: 3000,
+  ENV: ENV,
+  HMR: HMR
+};
+/*
+ * Config
+ * with default values at webpack.default.conf
+ */
 module.exports = {
-    entry: {
-        index: './src/tsx/CommentBox.tsx'
-    },
-    output: {
-        path: './public/scripts',
-        filename: '[name].min.js'
-    },
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM",
-        "jquery": "$",
-        "jquery": "jquery",
-        "lodash": "_"
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['', '.ts', '.js', '.tsx']
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new CopyWebpackPlugin([
-            { from: 'src/css/base.css', to: '../css' },
-            { from: 'src/html/index.html', to: '../' },
-            { from: 'public/components/jquery/dist/jquery.min.js', to: 'thirdparty' },
-            { from: 'public/components/marked/marked.min.js', to: 'thirdparty' },
-            { from: 'public/components/react/react.min.js', to: 'thirdparty' },
-            { from: 'public/components/react/react-dom.min.js', to: 'thirdparty' },
-            { from: 'public/components/bootstrap/dist/js/bootstrap.min.js', to: 'thirdparty' },
-            { from: 'public/components/lodash/dist/lodash.min.js', to: 'thirdparty' },
-            { from: 'public/components/bootstrap/dist/css/bootstrap.min.css', to: '../css' },
-            { from: 'public/components/font-awesome/css/font-awesome.min.css', to: '../css' },
-            { from: 'public/components/font-awesome/fonts', to: '../fonts' }
-        ])
+  // static data for index.html
+  metadata: metadata,
+  devtool: 'cheap-module-eval-source-map',
+  // cache: true,
+  debug: true,
+  // devtool: 'eval' // for faster builds use 'eval'
+
+  // our angular app
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+
+  // Config for our build files
+  output: {
+    path: helpers.root('dist'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  module: {
+    preLoaders: [
+      // { test: /\.ts$/, loader: 'tslint-loader', exclude: [ helpers.root('node_modules') ] },
+      // TODO(gdi2290): `exclude: [ helpers.root('node_modules/rxjs') ]` fixed with rxjs 5 beta.3 release
+      { test: /\.js$/, loader: "source-map-loader", exclude: [ helpers.root('node_modules/rxjs') ] }
     ],
-    module: {
-        loaders: [
-            {
-                test: /\.tsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'ts-loader'
-            }
-        ]
+    loaders: [
+      // Support for .ts files.
+      { test: /\.ts$/, loader: 'awesome-typescript-loader', exclude: [ /\.(spec|e2e)\.ts$/ ] },
+
+      // Support for *.json files.
+      { test: /\.json$/,  loader: 'json-loader' },
+
+      // Support for CSS as raw text
+      { test: /\.css$/,   loader: 'raw-loader' },
+
+      // support for .html as raw text
+      { test: /\.html$/,  loader: 'raw-loader', exclude: [ helpers.root('src/index.html') ] }
+
+    ]
+  },
+
+  plugins: [
+    new ForkCheckerPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({ name: ['app', 'vendor', 'polyfills'], minChunks: Infinity }),
+    // static assets
+    new CopyWebpackPlugin([ { from: 'src/assets', to: 'assets' } ]),
+    // generating html
+    new HtmlWebpackPlugin({ template: 'src/index.html', chunksSortMode: 'none' }),
+    // Environment helpers (when adding more properties make sure you include them in custom-typings.d.ts)
+    new webpack.DefinePlugin({
+      'ENV': JSON.stringify(metadata.ENV),
+      'HMR': HMR
+    })
+  ],
+
+  // Other module loader config
+
+  // our Webpack Development Server config
+  tslint: {
+    emitErrors: false,
+    failOnHint: false,
+    resourcePath: 'src',
+  },
+  devServer: {
+    port: metadata.port,
+    host: metadata.host,
+    historyApiFallback: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
     }
-}
+  },
+  node: {
+    global: 'window',
+    process: true,
+    crypto: 'empty',
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
+};
